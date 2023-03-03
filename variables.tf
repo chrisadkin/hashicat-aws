803ebd0b58d9ca902ceb5635d770b73d3e2bf5fb
Chris Adkin <chris@exadat.co.uk>
2023-03-03 15:45:13 +0000

    MySql username changed to Hashicorp

--- variables.tf
+++ variables.tf
@@ -31,7 +31,7 @@ variable "instance_type" {
 
 variable "admin_username" {
   description = "Administrator user name for mysql"
-  default     = "hasicat"
+  default     = "hasicorp"
 }
 
 variable "height" {
