dff280a4d5f5ad0b0baf91e35b7de3fbb4f70d02
Chris Adkin <chris@exadat.co.uk>
2023-03-03 15:06:10 +0000

    MySql admin password change to hashicat

--- variables.tf
+++ variables.tf
@@ -31,7 +31,7 @@ variable "instance_type" {
 
 variable "admin_username" {
   description = "Administrator user name for mysql"
-  default     = "hasicorp"
+  default     = "hasicat"
 }
 
 variable "height" {
